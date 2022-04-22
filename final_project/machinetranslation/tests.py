c0f1269e4caca7c381ba814ed0fe801d180b1de5
dracospitfire <austin3flores@gmail.com>
2022-04-22 04:39:15 +0000

    updated test for asserNotEqual

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -6,10 +6,14 @@ class TestEng2Fr(unittest.TestCase):
     def test1(self):
          self.assertEqual(englishToFrench('Hello'), 'Bonjour')
             #test case for the world Hello, shold translate to Bonjour
+         self.assertNotEqual(englishToFrench('Hello'), 'Hello')
+            #test case for the world Hello, shold translate to Bonjour
 
 class TestFr2Eng(unittest.TestCase):
      def test1(self):
           self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
              #test case for the world Bonjour, shold translate to Hello
+          self.assertNotEqual(frenchToEnglish('Bonjour'), 'Bonjour')
+            #test case for the world Hello, shold translate to Bonjour
 
 unittest.main()
