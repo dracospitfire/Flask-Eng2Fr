1317977287ee277f79650ead94c9568fe8408b65
dracospitfire <austin3flores@gmail.com>
2022-04-22 05:00:50 +0000

    updated spelling issues in test

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -5,15 +5,15 @@ from translator import englishToFrench, frenchToEnglish
 class TestEng2Fr(unittest.TestCase):
     def test1(self):
          self.assertEqual(englishToFrench('Hello'), 'Bonjour')
-            #test case for the world Hello, shold translate to Bonjour
+            #test case for the word "Hello", shold translate to Bonjour
          self.assertNotEqual(englishToFrench('Hello'), 'Hello')
-            #test case for the world Hello, function should not return Hello
+            #test case for the word "Hello", function should not return Hello
 
 class TestFr2Eng(unittest.TestCase):
      def test2(self):
           self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
-             #test case for the world Bonjour, shold translate to Hello
+             #test case for the word "Bonjour", shold translate to Hello
           self.assertNotEqual(frenchToEnglish('Bonjour'), 'Bonjour')
-            #test case for the world Bonjour, function shold not return Bonjour
+            #test case for the word "Bonjour", function shold not return Bonjour
 
 unittest.main()
