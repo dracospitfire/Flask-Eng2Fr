62113a45036f5a31b357a7fd2e37a29d8eceb18a
dracospitfire <austin3flores@gmail.com>
2022-04-22 03:30:42 +0000

    test file updated and all test pass

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,5 +1,4 @@
 """Modules for Watson Service"""
-import json
 import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -7,24 +6,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+apikey = os.environ['apikey']
+url = os.environ['url']
+myAuthenticator = IAMAuthenticator(apikey)
+
+language_translator = LanguageTranslatorV3(version = '2018-05-01', authenticator = myAuthenticator)
+language_translator.set_service_url(url)
 
 def english_2_french(english_text):
     """This function takes English text and translates it to French."""
-    apikey = os.environ['apikey']
-    url = os.environ['url']
-    VERSION='2018-05-01'
-    authenticator = IAMAuthenticator(apikey)
-    language_translator = LanguageTranslatorV3(version = VERSION, authenticator = authenticator)
-    language_translator.set_service_url(url)
     translation_response = language_translator.translate(text = english_text, model_id = 'en-fr')
     translation = translation_response.get_result()
     french_text = translation['translations'][0]['translation']
     return french_text
 
-
 def french_2_english(french_text):
     """This function takes French text and translates it to English."""
-    translation_response = language_translator.translate(text=french_text, model_id='fr-en')
+    translation_response = language_translator.translate(text = french_text, model_id='fr-en')
     translation=translation_response.get_result()
     english_text =translation['translations'][0]['translation']
     return english_text
