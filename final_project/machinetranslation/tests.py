103aa9d2b16b2ec8388cc04be1288a8d8854c5ad
dracospitfire <austin3flores@gmail.com>
2022-04-22 04:53:27 +0000

    comments did not discribe function and was corrected

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -7,13 +7,13 @@ class TestEng2Fr(unittest.TestCase):
          self.assertEqual(englishToFrench('Hello'), 'Bonjour')
             #test case for the world Hello, shold translate to Bonjour
          self.assertNotEqual(englishToFrench('Hello'), 'Hello')
-            #test case for the world Hello, shold translate to Bonjour
+            #test case for the world Hello, function should not return Hello
 
 class TestFr2Eng(unittest.TestCase):
      def test2(self):
           self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
              #test case for the world Bonjour, shold translate to Hello
           self.assertNotEqual(frenchToEnglish('Bonjour'), 'Bonjour')
-            #test case for the world Hello, shold translate to Bonjour
+            #test case for the world Bonjour, function shold not return Bonjour
 
 unittest.main()
